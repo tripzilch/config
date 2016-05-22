659f7e82089afd4b2d90c6af1516b50779733890
tripzilch <pseudoscalar@gmail.com>
2016-05-22 18:35:46 +0200

    added bookmarklets. large rewrite of linux-packages.md. clonepc image notes more fleshed out.

---
+++ fixedfixer.bookmarklet.js
@@ -0,0 +1,1 @@
+javascript:Array.forEach(document.getElementsByTagName('*'),function(e){if(/fixed|sticky/.test(getComputedStyle(e).position))e.style.position='static';})
---
+++ next.bookmarklet.js
@@ -0,0 +1,1 @@
+javascript:['a[rel=next]','a.next','a[class*=next]','a[title*=next]','.next>a','[class*=next]>a'].find(function(v){var%20e=document.querySelector(v);if(e){e.click();return%201}else%20return%200});
